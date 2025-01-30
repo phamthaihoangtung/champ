ac2ca254e5e5feb87161d6046172908afb41a1ef
phamthaihoangtung <phamthaihoangtung@gmail.com>
2025-01-30 16:54:04 +0700

    modify control to work in gz-sim

--- champ_description/launch/description.launch.py
+++ champ_description/launch/description.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
-
+import xacro
 
 def generate_launch_description():
 
@@ -18,6 +18,9 @@ def generate_launch_description():
 
     pkg_share = launch_ros.substitutions.FindPackageShare(package="champ_description").find("champ_description")
     default_model_path = os.path.join(pkg_share, "urdf/champ.urdf.xacro")
+    
+    doc = xacro.parse(open(default_model_path))
+    xacro.process_doc(doc)
 
     declare_description_path = DeclareLaunchArgument(name="description_path", default_value=default_model_path, description="Absolute path to robot urdf file")
 
@@ -26,7 +29,8 @@ def generate_launch_description():
         executable="robot_state_publisher",
         
         parameters=[
-            {"robot_description": Command(["xacro ", description_path])},
+            # {"robot_description": Command(["xacro ", description_path])},
+            {"robot_description": doc.toxml()},
             {"use_tf_static": False},
             {"publish_frequency": 200.0},
             {"ignore_timestamp": True},
